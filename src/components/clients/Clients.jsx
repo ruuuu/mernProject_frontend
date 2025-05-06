2f63acdcc695099dae1dbe33bc595104751a77d0
ruuuu <rufinka_91@mail.ru>
2025-05-06 17:20:25 +0300

    add table for clients

--- src/components/clients/Clients.jsx
+++ src/components/clients/Clients.jsx
@@ -6,11 +6,11 @@ import axios from 'axios';
 // компонент
 const Clients = () => {
 
-  const [ clients, setClients ] = useState([]); // завели перем состояния clients
+  const [ clients, setClients ] = useState([]); // хук,  завели перем состояния clients компонета
 
 
 
-  useEffect(() => {
+  useEffect(() => { // хук
 
     axios.get('http://localhost:5000/api/clients')
       .then(response => {
@@ -18,15 +18,38 @@ const Clients = () => {
         setClients(response.data); // устаналвиваем обновленное значение в перем clients
       })
 
-  }, []);
+  }, clients, []); //  при первичном ренере отправится запрос на сервер, при изменении clients, запутсится переданный колбэк и перерисуется компонент
 
 
 
   return (
     <div> 
-      { clients.map(elem => (
-        <p key={elem.id}> {elem.fio} </p>
-      ))}
+      <table>
+        <thead>
+          <td>ФИО</td>
+          <td>Дата покупки</td>
+          <td>Первое посещение</td>
+          <td>Последнее посещение</td>
+          <td>Телефон</td>
+          <td>Срок действия абонемента</td>
+          <td>Статус</td>
+          <td>Тренер</td>
+        </thead>
+        
+        { clients.map(elem => (
+            <tr key={elem.id}>
+              <td> {elem.fio} </td>
+              <td> {elem.dateBuy} </td>
+              <td> {elem.firstCame} </td>
+              <td> {elem.lastCame} </td>
+              <td> {elem.phone} </td>
+              <td> {elem.srok} </td>
+              <td> {elem.status} </td>
+              <td> {elem.trenerId} </td>
+            </tr> 
+        ))}
+        
+      </table> 
     </div>
   );
 };
